88176c7774fda1a8b613cd1df77a12e1722129b1
brandonhuang <hi@brandonhuang.io>
2015-08-07 23:40:02 -0700

    prevent spaces from being submitted

--- client/templates/channel/message_form/message_form.js
+++ client/templates/channel/message_form/message_form.js
@@ -1,15 +1,28 @@
 Template.messageForm.events({
   'submit form': function(e) {
+    e.preventDefault();
+
     var message = {
       text: $('#m').val()
     }
 
     $('#m').val("");
 
+    // Validate
+    if(isEmpty(message.text)) return;
+
+
+
     Meteor.call('messageInsert', message, function(error) {
       if(error) throw error;
     });
-
-    return false;
   }
 });
+
+function isEmpty(text) {
+  if(text.trim()) {
+    return false;
+  } else {
+    return true;
+  }
+}
