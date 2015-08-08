d44924f1998d812ef640258234ddc4e778f1c8c4
brandonhuang <hi@brandonhuang.io>
2015-08-07 23:54:36 -0700

    added message throttling

--- client/templates/channel/message_form/message_form.js
+++ client/templates/channel/message_form/message_form.js
@@ -1,3 +1,5 @@
+var msgCount = 0;
+
 Template.messageForm.events({
   'submit form': function(e) {
     e.preventDefault();
@@ -10,14 +12,27 @@ Template.messageForm.events({
 
     // Validate
     if(isEmpty(message.text)) return;
+    if(isTooFast(msgCount)) return;
 
-
+    msgCount++;
 
     Meteor.call('messageInsert', message, function(error) {
       if(error) throw error;
     });
   }
 });
+
+setInterval(function() {
+  if(msgCount > 0) msgCount--;
+}, 3000);
+
+function isTooFast(msgCount) {
+  if(msgCount > 3) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
 function isEmpty(text) {
   if(text.trim()) {
