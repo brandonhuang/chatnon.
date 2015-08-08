82ffc9c566d35d9a4b6ffabbfd80e3f5187d3dbb
brandonhuang <hi@brandonhuang.io>
2015-08-08 00:47:58 -0700

    added more spam filter/validations

--- client/templates/channel/message_form/message_form.js
+++ client/templates/channel/message_form/message_form.js
@@ -1,4 +1,5 @@
 var msgCount = 0;
+var msgCache = [];
 
 Template.messageForm.events({
   'submit form': function(e) {
@@ -13,8 +14,10 @@ Template.messageForm.events({
     // Validate
     if(isEmpty(message.text)) return;
     if(isTooFast(msgCount)) return;
+    if(isRepetitive(message.text)) return;
 
     msgCount++;
+    cacheMessage(message.text);
 
     Meteor.call('messageInsert', message, function(error) {
       if(error) throw error;
@@ -24,7 +27,17 @@ Template.messageForm.events({
 
 setInterval(function() {
   if(msgCount > 0) msgCount--;
+  console.log(msgCache);
 }, 3000);
+
+function cacheMessage(text) {
+  if(msgCache.length < 3) {
+    msgCache.push(text);
+  } else {
+    msgCache.shift();
+    msgCache.push(text);
+  }
+}
 
 function isTooFast(msgCount) {
   if(msgCount > 3) {
@@ -40,4 +53,18 @@ function isEmpty(text) {
   } else {
     return true;
   }
+}
+
+function isRepetitive(text) {
+  if(msgCache.length === 3) {
+    for(var i = 0; i < msgCache.length; i++) {
+      if(msgCache[i] !== msgCache[0]) {
+        return false;
+      }
+    }
+    msgCount += 5;
+    msgCache.shift();
+    return true;
+  }
+  return false;
 }
