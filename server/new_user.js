d28c6460b8b5ad5a6c8959374698e89cfd0e09dc
brandonhuang <hi@brandonhuang.io>
2015-08-10 16:02:18 -0700

    usernames are now case-insensitive

--- server/new_user.js
+++ server/new_user.js
@@ -1,14 +1,22 @@
+Accounts.validateNewUser(function (user) {
+  if(user.username.length > 14) {
+    throw new Meteor.Error(403, "Username must be shorter than 14 characters");
+  }
+  if(Meteor.users.findOne({usernameLower: user.username.toLowerCase()})) {
+    throw new Meteor.Error(403, "Username already exists");
+  }
+
+  return true
+});
+
 Accounts.onCreateUser(function(options, user) {
   user.color = generateColor();
   user.level = 1;
   user.exp = 0;
   user.location = {};
 
-  // Turnicate usernames if greater than 14
-  if(user.username.length > 14) {
-    console.log(user.username)
-    user.username = user.username.slice(0, 14);
-  }
+  // Create a lower case version of username
+  user.usernameLower = user.username.toLowerCase();
 
   if(options.profile) {
     user.profile = options.profile;
