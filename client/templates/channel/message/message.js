ea93092f66d7aee3404364691fd682995c80605d
brandonhuang <hi@brandonhuang.io>
2015-08-10 19:17:25 -0700

    mute functionality added

--- client/templates/channel/message/message.js
+++ client/templates/channel/message/message.js
@@ -30,3 +30,21 @@ Template.message.helpers({
     return Meteor.users.findOne({username: this.username}).level;
   }
 });
+
+Meteor.startup(function() {
+  Session.setDefault('muteList', []);
+});
+
+Template.message.events({
+  'click .tag': function(e) {
+    if(Meteor.user().username === this.username) return;
+    if(!confirm('Mute this user?')) return;
+
+    // Clone array
+    var muteList = Session.get("muteList").slice();
+
+    if(muteList.indexOf(this.username) == -1) muteList.push(this.username);
+
+    Session.set('muteList', muteList);
+  }
+});
