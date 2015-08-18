150c6843787ca51c19389be3aa636573c325f178
brandonhuang <hi@brandonhuang.io>
2015-08-17 21:21:02 -0700

    channel optimizations

--- server/userChannel.js
+++ server/userChannel.js
@@ -2,9 +2,9 @@
 // The rest of the code is contained in the router.js
 
 Accounts.onLogin(function(data) {
-
   data.connection.onClose(function() {
     var channel = UserSession.get(data.connection.id, data.user._id);
+    UserSession.delete(data.connection.id, data.user._id);
     if(channel === undefined || channel === 'undefined') return;
     Meteor.call('disconnectChannel', channel, data.user._id);
   });
