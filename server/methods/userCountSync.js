368b374a852ad896e6d8eee312289aea9c861e8a
brandonhuang <hi@brandonhuang.io>
2015-08-30 17:44:57 -0700

    app syncs users on start

--- server/methods/userCountSync.js
+++ server/methods/userCountSync.js
@@ -1,4 +1,18 @@
 Meteor.startup(function() {
+  Meteor.setTimeout(function() {
+    var openChannels = channels.find({});
+
+    openChannels.forEach(function(channel) {
+      var obj = {};
+          obj['channels.' + channel.name + '.online'] = true;
+      var usersOnline = Meteor.users.find(obj).count();
+
+      channels.update(channel._id, {$set: {usersOnline: usersOnline}});
+    });
+
+    console.log('channel userCounts synced');
+  }, 1000);
+
   Meteor.setInterval(function() {
     var openChannels = channels.find({});
 
