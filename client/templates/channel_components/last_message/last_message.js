33765fc6382f754ab9861a4cc0e2115206cc6b27
brandonhuang <hi@brandonhuang.io>
2015-09-08 22:04:12 -0700

    included singular times

--- client/templates/channel_components/last_message/last_message.js
+++ client/templates/channel_components/last_message/last_message.js
@@ -5,7 +5,9 @@ Template.lastMessage.helpers({
 
     if(milliseconds < 5000) return 'just now';
     if(milliseconds < 60000) return milliseconds.getSeconds() + ' seconds ago';
+    if(milliseconds < 120000) return milliseconds.getMinutes() + ' minute ago'
     if(milliseconds < 3600000) return milliseconds.getMinutes() + ' minutes ago';
+    if(milliseconds < 7200000) return milliseconds.getHours() + 'hour ago'
     return milliseconds.getHours() + ' hours ago';
   }
 });
