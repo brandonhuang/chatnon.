463faae4424c7a3589d3c5283151060359f1bd4f
brandonhuang <hi@brandonhuang.io>
2015-09-08 21:56:02 -0700

    added for all message_forms

--- client/templates/channel_components/last_message/last_message.js
+++ client/templates/channel_components/last_message/last_message.js
@@ -1,14 +1,11 @@
 Template.lastMessage.helpers({
   time: function() {
-    var lastMessage = messages.findOne({}, {sort: {createdAt: -1}});
+    var lastMessage = messages.findOne({channel: Session.get('channel')}, {sort: {createdAt: -1}});
     var milliseconds = new Date(new Date(TimeSync.serverTime(null, 1000)) - lastMessage.createdAt);
 
-    if(milliseconds < 60000) {
-      return milliseconds.getSeconds() + ' seconds';
-    } else if(milliseconds < 3600000) {
-      return milliseconds.getMinutes() + ' minutes';
-    } else {
-      return milliseconds.getHours() + ' hours'
-    }
+    if(milliseconds < 1000) return 'just now';
+    if(milliseconds < 60000) return milliseconds.getSeconds() + ' seconds ago';
+    if(milliseconds < 3600000) return milliseconds.getMinutes() + ' minutes ago';
+    return milliseconds.getHours() + ' hours ago';
   }
 });
