fd4025d348f2baf6106c53a9992d8ff283ce267e
brandonhuang <hi@brandonhuang.io>
2015-09-08 21:58:25 -0700

    small tweak to just now

--- client/templates/channel_components/last_message/last_message.js
+++ client/templates/channel_components/last_message/last_message.js
@@ -3,7 +3,7 @@ Template.lastMessage.helpers({
     var lastMessage = messages.findOne({channel: Session.get('channel')}, {sort: {createdAt: -1}});
     var milliseconds = new Date(new Date(TimeSync.serverTime(null, 1000)) - lastMessage.createdAt);
 
-    if(milliseconds < 1000) return 'just now';
+    if(milliseconds < 5000) return 'just now';
     if(milliseconds < 60000) return milliseconds.getSeconds() + ' seconds ago';
     if(milliseconds < 3600000) return milliseconds.getMinutes() + ' minutes ago';
     return milliseconds.getHours() + ' hours ago';
