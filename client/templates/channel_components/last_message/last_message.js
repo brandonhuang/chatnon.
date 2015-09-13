6bb265531ef5da3f9ce0b7b821fea7b89cc2e009
brandonhuang <hi@brandonhuang.io>
2015-09-13 00:18:54 -0700

    fixed bug

--- client/templates/channel_components/last_message/last_message.js
+++ client/templates/channel_components/last_message/last_message.js
@@ -5,9 +5,9 @@ Template.lastMessage.helpers({
 
     if(milliseconds < 5000) return 'just now';
     if(milliseconds < 60000) return milliseconds.getSeconds() + ' seconds ago';
-    if(milliseconds < 120000) return milliseconds.getMinutes() + ' minute ago'
+    if(milliseconds < 120000) return '1 minute ago';
     if(milliseconds < 3600000) return milliseconds.getMinutes() + ' minutes ago';
-    if(milliseconds < 7200000) return milliseconds.getHours() + ' hour ago'
+    if(milliseconds < 7200000) return '1 hour ago';
     return Math.round(milliseconds/3600000) + ' hours ago';
   }
 });
